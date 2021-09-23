d66075b416474e8affe5dac6ce1ef22714b722db
김정윤 <kjyjh9930@gmail.com>
2021-09-24 00:27:59 +0900

    init project

---
+++ src/main/java/com/jy/hiSleep/HiSleepApplication.java
@@ -0,0 +1,13 @@
+package com.jy.hiSleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiSleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiSleepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jy/hiSleep/HiSleepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.hiSleep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiSleepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
