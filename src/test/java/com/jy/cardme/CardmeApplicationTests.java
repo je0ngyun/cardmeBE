502e336990dc50c15db93d312a18545170328218
김정윤 <kjyjh9930@gmail.com>
2021-10-01 11:12:52 +0900

    modify : 테스트 컴포넌트 설정파일 연결

--- src/test/java/com/jy/cardme/CardmeApplicationTests.java
+++ src/test/java/com/jy/cardme/CardmeApplicationTests.java
@@ -3,7 +3,9 @@ package com.jy.cardme;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.config.location=" +
+		"/config/application.yml"
+)
 class CardmeApplicationTests {
 
 	@Test
