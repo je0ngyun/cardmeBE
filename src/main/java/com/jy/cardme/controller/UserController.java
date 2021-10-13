e8ca25676cc8eb9af6f4bb5d1caaae05db07bb5d
김정윤 <kjyjh9930@gmail.com>
2021-10-14 00:46:27 +0900

    fix : 누락 파일 추가

---
+++ src/main/java/com/jy/cardme/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.jy.cardme.controller;
+
+import com.jy.cardme.components.DefaultRes;
+import com.jy.cardme.components.ResponseMessage;
+import com.jy.cardme.components.StatusCode;
+import com.jy.cardme.dto.UserInfoDto;
+import com.jy.cardme.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity getUserInfo(@PathVariable("userId") String userId) {
+        final Object data = userService.getUserInfo(
+                UserInfoDto.builder().userId(userId).build());
+        final DefaultRes res = DefaultRes.builder()
+                .message(ResponseMessage.READ_USER)
+                .httpStatus(StatusCode.OK)
+                .timestamp(ZonedDateTime.now(ZoneId.of("Z")))
+                .data(data).build();
+        return new ResponseEntity(res, HttpStatus.OK);
+    }
+}
